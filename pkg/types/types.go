06f2af50b986d907cd5489aaaa98341d88afbf56
Shamshod <Shamshod@go.alif.academy>
2021-04-02 21:59:05 +0500

    edit types

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -1,25 +1,53 @@
 package types
 
-// Money представляет собой денеэную сумму в минималных единиц (центы, копейки, дирамы и т.д).
+// Money представляет собой денежную сумму в минимальных единицах
 type Money int64
 
-// Category представляет собой категорию, в которию был совершен платёж (авто, аптека, рестараны и т.д).
+// Category представляет собой категорию, в которой был совершён платёж ( авто, аптеки, рестораны и т.д.).
 type Category string
 
-// Status представляет статуч плятежей.
+// Status представляет собой статус платежа
 type Status string
 
-// Предопределеные статусы платежа.
+// Предопределённые статусы платежей.
 const (
-	StatusOk       Status = "OK"
-	StatusFail     Status = "FAIL"
-	StatusProgress Status = "INPROGRESS"
+	StatusOk Status = "OK"
+	StatusFail Status = "FAIL"
+	StatusInProgress Status = "INPROGRESS"
 )
 
-// Payment представляет игформацию о платиже.
+//  Currency  представляет код валюты.
+type Currency string 
+// Коды валют
+ const (
+  	TJS Currency = "TJS"
+  	RUB Currency = "RUB"
+  	USD Currency = "USD"
+  )
+
+// PAN представляет номер карты.
+type PAN string
+
+// Card представляет информацию о платёжной карте.
+type Card struct {
+	ID         int
+	PAN        PAN
+	Balance    Money
+	MinBalance Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+}
+// Payment представляет информацию о платеже
 type Payment struct {
-	ID       int
-	Amount   Money
+	ID      int
+	Amount  Money
 	Category Category
-	Status   Status
+	Status Status
+}
+type PaymentSourse struct {
+	Type        string //'card'
+	Number      string // номер вида '5058 xxxx xxxx 8888'
+	Balance     Money // баланс в дирамах
 }
